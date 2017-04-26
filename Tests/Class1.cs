2729c22e7000e8e721c2f977503283d626e9545a
James Wright <james.wright@red-gate.com>
2017-04-26 15:09:06 +0100

    [Red] First test

---
+++ Tests/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Can_Leave_Review()
+        {
+            var reviewCollection = new ReviewCollection();
+            var review = new Review("The Abyss", 4, "James Wright", "Pretty good");
+            Assert.DoesNotThrow(() =>
+            {
+                reviewCollection.Add(review);
+            });
+        }
+    }
+}
